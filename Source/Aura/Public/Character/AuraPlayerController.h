9acedf3450597473ce0031a9366a023d4b2dc25e
AttackOG <cullenj213@gmail.com>
2024-06-30 16:51:15 +0100

    Enhanced Input

---
+++ Source/Aura/Public/Character/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// GamePlayAbilitySystem course followed by AttackOG. Course was created by Stephen Ulibarri, founder of Druid Mechanics. Thank you!
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
