fc9ed0926037ca1261e4f2ffc4c6d5f451d9afdf
AttackOG <cullenj213@gmail.com>
2024-06-30 17:10:55 +0100

    Player Controller

--- Source/Aura/Private/Character/AuraPlayerController.cpp
+++ Source/Aura/Private/Character/AuraPlayerController.cpp
@@ -2,4 +2,26 @@
 
 
 #include "Character/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
 
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(AuraContext);
+	UEnhancedInputLocalPlayerSubsystem* LocalPlayerSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(LocalPlayerSubsystem);
+	LocalPlayerSubsystem->AddMappingContext(AuraContext, 0 );
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
