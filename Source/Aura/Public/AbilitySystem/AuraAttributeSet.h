ec4516c9fc5e82a42c077dc334dbb4dadc52e7e8
AttackOG <cullenj213@gmail.com>
2024-07-02 16:53:44 +0100

    Health and Mana

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "Net/UnrealNetwork.h"
 #include "AuraAttributeSet.generated.h"
 
 /**
@@ -13,5 +14,27 @@ UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	UAuraAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(Replicated, ReplicatedUsing = OnRep_Health)
+	FGameplayAttributeData Health;
+	UPROPERTY(Replicated, ReplicatedUsing = OnRep_MaxHealth)
+	FGameplayAttributeData MaxHealth;
+	UPROPERTY(Replicated, ReplicatedUsing = OnRep_Mana)
+	FGameplayAttributeData Mana;
+	UPROPERTY(Replicated, ReplicatedUsing = OnRep_MaxMana)
+	FGameplayAttributeData MaxMana;
+
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth);
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana);
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana);
 	
 };
