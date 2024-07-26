f046dedf667ab40fed8112d4c071b668fda6bef0
AttackOG <cullenj213@gmail.com>
2024-07-26 20:07:40 +0100

    AuraAssetManager

---
+++ Source/Aura/Private/AuraAssetManager.cpp
@@ -0,0 +1,19 @@
+// GamePlayAbilitySystem course followed by AttackOG. Course was created by Stephen Ulibarri, founder of Druid Mechanics. Thank you!
+
+
+#include "AuraAssetManager.h"
+
+#include "AuraGameplayTags.h"
+
+UAuraAssetManager& UAuraAssetManager::Get()
+{
+	check(GEngine);
+	UAuraAssetManager* AuraAssetManager = Cast<UAuraAssetManager>(GEngine->AssetManager);
+	return *AuraAssetManager;
+}
+
+void UAuraAssetManager::StartInitialLoading()
+{
+	Super::StartInitialLoading();
+	FAuraGameplayTags::InitializeNativeGameplayTags();
+}
---
+++ Source/Aura/Public/AuraAssetManager.h
@@ -0,0 +1,23 @@
+// GamePlayAbilitySystem course followed by AttackOG. Course was created by Stephen Ulibarri, founder of Druid Mechanics. Thank you!
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "AuraAssetManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+
+public:
+	static UAuraAssetManager& Get();
+
+protected:
+	virtual void StartInitialLoading() override;
+	
+};
